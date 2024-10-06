58474a49dfd6a341a707e7ef23702d325f56f709
Luciano Boteleiro <lucianomalone@gmail.com>
2024-10-06 20:13:22 -0300

    mudança de avatar

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function toggleMode(){
   const img = document.querySelector("#profile img")
   //substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute('src','./assets/avatar-light.png')
+    img.setAttribute('src','./assets/Avatardia.png')
     img.setAttribute('alt','Foto com Oculos de sol')
     
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatarnoite.png")
     img.setAttribute("alt", "Foto com Oculos de grau")
   }
 
